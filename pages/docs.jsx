3a1c07cb49e9d4819275cd55fa68348fd49ee20b
DawidWraga <dpwraga@gmail.com>
2022-11-01 16:40:20 +0000

    remove old code from docs

--- pages/docs.jsx
+++ pages/docs.jsx
@@ -1,15 +1,5 @@
-import { Flex } from '@chakra-ui/react';
-import { posts } from 'db/posts';
-import { Card } from 'components/card';
-
-export default function DocsPage(props) {
+xport default function DocsPage(props) {
 	const {} = props;
 
-	return (
-		<Flex h="50vh" alignContent={'flex-start'}>
-			{posts.map((card) => (
-				<Card title={card.title} key={card.title} src={card.src} />
-			))}
-		</Flex>
-	);
+	return <>test</>;
 }
