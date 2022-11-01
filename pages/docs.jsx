e917d43cdcd9ea3f0f802fd88f46859a6cc1746e
[Dawid Wraga] <[dpwraga@gmail.com]>
2022-11-01 23:15:09 +0000

    refactor

--- pages/docs.jsx
+++ pages/docs.jsx
@@ -1,65 +1,56 @@
-import {
-  Box,
-  ButtonGroup,
-  Button,
-  Flex,
-  Spacer,
-  Center,
-  Text,
-} from '@chakra-ui/react';
-// import Button from './Button';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
 
 import topics from 'db/postTopics.json';
 
 export default function DocsPage(props) {
-  const {} = props;
+	const {} = props;
 
-  return (
-    //HTML BELOW
+	return (
+		//HTML BELOW
 
-    <Box>
-      <Box>
-        {topics.map((topic) => (
-          //map through JSON
+		<Box>
+			<Box>
+				{topics.map((topic) => (
+					//map through JSON
 
-          <Flex
-            bgColor="blue.800"
-            color="whiteAlpha.900"
-            p="3px"
-            maxW="200px"
-            gap="3"
-          >
-            <Button
-              p="3"
-              variant={'ghost'}
-              w="100%"
-              h="100%"
-              textAlign={'left'}
-              alignSelf={'center'}
-              colorScheme={'whiteAlpha'}
-              color="white"
-            >
-              <Text mr={'auto'} textAlign="left">
-                {topic.name}
-              </Text>
-            </Button>
+					<Flex
+						bgColor="blue.800"
+						color="whiteAlpha.900"
+						p="3px"
+						maxW="200px"
+						gap="3"
+					>
+						<Button
+							p="3"
+							variant={'ghost'}
+							w="100%"
+							h="100%"
+							textAlign={'left'}
+							alignSelf={'center'}
+							colorScheme={'whiteAlpha'}
+							color="white"
+						>
+							<Text mr={'auto'} textAlign="left">
+								{topic.name}
+							</Text>
+						</Button>
 
-            {/* numPosts box */}
-            <Box
-              p="1"
-              minW="3ch"
-              bg=""
-              color="whiteAlpha.900"
-              alignSelf={'center'}
-              textAlign={'center'}
-            >
-              {topic.numPosts}
-            </Box>
-          </Flex>
-        ))}
-      </Box>
-    </Box>
-  );
+						{/* numPosts box */}
+						<Box
+							p="1"
+							minW="3ch"
+							bg=""
+							color="whiteAlpha.900"
+							alignSelf={'center'}
+							textAlign={'center'}
+						>
+							{topic.numPosts}
+						</Box>
+					</Flex>
+				))}
+			</Box>
+		</Box>
+	);
 }
 
 // create card
