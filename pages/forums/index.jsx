98db19b2f07f8a8a78e355980bc666c5ba86b6dc
lsheldon02 <l.sheldon-21@student.lboro.ac.uk>
2022-11-02 15:16:32 +0000

    Moved Post, form components into components

--- pages/forums/index.jsx
+++ pages/forums/index.jsx
@@ -1,74 +1,23 @@
 import {
   Box,
-  ButtonGroup,
   Button,
-  Center,
-  Image,
-  Heading,
-  Text,
   Flex,
-  Avatar,
-  AvatarGroup,
-  Link,
+  Input,
+  FormLabel,
+  FormControl,
 } from '@chakra-ui/react';
-import { title } from 'process';
-import { toast } from 'react-toastify';
 import { posts } from 'db/posts';
-import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { Post } from 'components/Post';
+import { AddPostForm } from 'components/AddPostForm';
 // import Button from './Button';
 
 export default function ForumsPage(props) {
   const {} = props;
+  const [formActive, setFormActive] = useState(false);
 
-  function Post(props) {
-    const { post } = props;
-    const { title, name, replies, timesince, usericon, solved, id } = post;
-    const router = useRouter();
-
-    return (
-      <Flex
-        backgroundColor={'white'}
-        //minW="50vw"
-        w="clamp(350px,80vw,750px)"
-        minH={'80px'}
-        py="2"
-        px="4"
-        align={'center'}
-        rounded="sm"
-      >
-        <Avatar size={'md'} src={usericon}></Avatar>
-        <Flex fontSize={'sm'} flexDirection={'column'} flexGrow="1" padding="2">
-          <Text
-            as={Link}
-            onClick={() => {
-              router.push('/forums/' + id);
-              console.log('test');
-            }}
-            _hover={{
-              textColor: 'blue.500',
-            }}
-            fontWeight={'semibold'}
-            fontSize={'md'}
-            noOfLines={1}
-          >
-            {title}
-          </Text>
-          <Flex justifyContent={'left'} alignItems={'center'} gap="4">
-            <Text>{name}</Text>
-            <Text>{timesince} ago</Text>
-            <Text>{solved ? 'Solved' : 'Unsolved'}</Text>
-          </Flex>
-        </Flex>
-        <Box align="center">
-          <AvatarGroup size={'sm'} max={2}>
-            <Avatar src={usericon} />
-            <Avatar src={usericon} />
-            <Avatar src={usericon} />
-          </AvatarGroup>
-          <Text fontSize={'sm'}>{replies} replies</Text>
-        </Box>
-      </Flex>
-    );
+  function toggleForm() {
+    setFormActive((formActive) => !formActive);
   }
 
   return (
@@ -84,6 +33,8 @@ export default function ForumsPage(props) {
       {posts.map((post) => (
         <Post post={post} key={post.id} />
       ))}
+      <Button onClick={toggleForm}>Show/Hide form</Button>
+      {formActive && <AddPostForm />}
     </Flex>
   );
 }
