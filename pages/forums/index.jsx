c97137a36c41dad11227312d7f33f1d50185c4b5
lsheldon02 <l.sheldon-21@student.lboro.ac.uk>
2022-11-02 16:28:17 +0000

    Added modal/create post button

--- pages/forums/index.jsx
+++ pages/forums/index.jsx
@@ -1,24 +1,30 @@
 import {
-  Box,
   Button,
   Flex,
-  Input,
-  FormLabel,
-  FormControl,
+  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
 } from '@chakra-ui/react';
 import { posts } from 'db/posts';
-import { useState } from 'react';
 import { Post } from 'components/Post';
-import { AddPostForm } from 'components/AddPostForm';
+import { HiPlus as Plus } from 'react-icons/hi';
 // import Button from './Button';
 
 export default function ForumsPage(props) {
   const {} = props;
-  const [formActive, setFormActive] = useState(false);
+  // const [formActive, setFormActive] = useState(false);
 
-  function toggleForm() {
-    setFormActive((formActive) => !formActive);
-  }
+  // function toggleForm() {
+  //   setFormActive((formActive) => !formActive);
+  // }
+
+  const { isOpen, onOpen, onClose, onPost } = useDisclosure();
 
   return (
     <Flex
@@ -33,8 +39,34 @@ export default function ForumsPage(props) {
       {posts.map((post) => (
         <Post post={post} key={post.id} />
       ))}
-      <Button onClick={toggleForm}>Show/Hide form</Button>
-      {formActive && <AddPostForm />}
+      {/* <Button onClick={toggleForm}>Show/Hide form</Button> }
+      {formActive && <AddPostForm />} */}
+      <IconButton
+        onClick={onOpen}
+        colorScheme={'brand'}
+        icon={<Plus />}
+        position={'fixed'}
+        bottom={'10'}
+        right={'10'}
+      />
+      <Modal
+        size={'xl'}
+        motionPreset="slideInBottom"
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create Post</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>InputGoesHere</ModalBody>
+          <ModalFooter>
+            <Button onClick={onPost} colorScheme={'brand'}>
+              Post
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Flex>
   );
 }
