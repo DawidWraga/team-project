19cf58e9726cd7a8e10cbbf39b7686998a505baf
lsheldon02 <l.sheldon-21@student.lboro.ac.uk>
2022-11-03 11:39:45 +0000

    Improved forum post page

--- pages/forums/[id].js
+++ pages/forums/[id].js
@@ -1,31 +1,38 @@
 import { useRouter } from 'next/router';
 import { posts } from 'db/posts';
-import { Avatar, Box, Flex, Heading, Text, Button } from '@chakra-ui/react';
+import {
+  Avatar,
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Button,
+  Spacer,
+} from '@chakra-ui/react';
 
 export default function ForumPost(props) {
-	const {} = props;
+  const {} = props;
 
-	const router = useRouter();
-	const { id } = router.query;
+  const router = useRouter();
+  const { id } = router.query;
 
-	const post = posts.find((item) => +id === item.id);
-	if (!post) return <div>No post</div>;
+  const post = posts.find((item) => +id === item.id);
+  if (!post) return <div>No post</div>;
 
   return (
-    <Box backgroundColor={'white'} py="2" px="4">
+    <Box backgroundColor={'white'} py="4" px="4">
       <Flex alignItems={'center'} gap="4px">
         <Avatar size={'md'}></Avatar>
         <Text>{post.name}</Text>
-        <Flex>
-          <Text>Test</Text>
-        </Flex>
+        <Spacer />
+        <Text paddingRight={'4'}>{post.timesince} ago</Text>
       </Flex>
       <Box>
         <Text fontWeight={'bold'} fontSize="2xl">
           {post.title}
         </Text>
         <Text>{post.desc}</Text>
-        <Button>Reply</Button>
+        <Button colorScheme={'brand'}>Reply</Button>
       </Box>
     </Box>
   );
