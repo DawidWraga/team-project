b73dcc5d8a84bbe14b5e17f8094fa4843191a107
anjanamadhavram <anjanamadhavram@gmail.com>
2022-11-06 18:22:08 +0000

    debug dashboard import

--- components/dashboards/UserDashboard.jsx
+++ components/dashboards/UserDashboard.jsx
@@ -3,38 +3,42 @@ import { LineChart, getLineChartDummyData } from 'components/charts/LineChart';
 import { PieChart } from 'components/charts/PieChart';
 import { UserTasksList } from 'components/dashboards/UserTasksList';
 import { randomNum } from 'utils/randomNum';
+import { Paper } from 'styles/Paper';
+import { PageWrapper } from 'styles/PageWrapper';
 
 export function UserDashboard(props) {
-	const {} = props;
+  const {} = props;
 
-	const lineChartData = getLineChartDummyData();
+  const lineChartData = getLineChartDummyData();
 
-	const pieChartData = [
-		{
-			name: 'not started',
-			value: randomNum(0, 10),
-		},
-		{
-			name: 'in progress',
-			value: randomNum(0, 10),
-		},
-		{
-			name: 'review',
-			value: randomNum(0, 10),
-		},
-		{
-			name: 'done',
-			value: randomNum(0, 10),
-		},
-	];
+  const pieChartData = [
+    {
+      name: 'not started',
+      value: randomNum(0, 10),
+    },
+    {
+      name: 'in progress',
+      value: randomNum(0, 10),
+    },
+    {
+      name: 'review',
+      value: randomNum(0, 10),
+    },
+    {
+      name: 'done',
+      value: randomNum(0, 10),
+    },
+  ];
 
-	return (
-		<div>
-			<LineChart data={lineChartData} />
-			<PieChart pieChartData={pieChartData} />
-			<UserTasksList tasks={tasks} />
-		</div>
-	);
+  return (
+    <div>
+      <Paper w="500px">
+        <LineChart data={lineChartData} />
+      </Paper>
+      <PieChart pieChartData={pieChartData} />
+      <UserTasksList tasks={tasks} />
+    </div>
+  );
 }
 
 // const tasks = [
