2555f25c7058343a8af0785b8dc37e912f97128c
[Dawid Wraga] <[dpwraga@gmail.com]>
2022-11-06 18:26:20 +0000

    debug addReply

--- pages/api/addReply.js
+++ pages/api/addReply.js
@@ -2,27 +2,27 @@ import query from 'controllers/query';
 import { writeToDb } from 'controllers/writeToDb';
 import { v4 as uuidv4 } from 'uuid';
 
-
 export default async function handler(req, res) {
-  try {
-    const router = useRouter();
-    const { routeid } = router.query;
-    console.log(routeid)
-    console.log(req.body);
-    const comment = {
-      ...req.body,
-      id: routeid,
-      timesince: '0 days',
-      replies: '0',
-      solved: false,
-    };
-    const comments = await query('postComments');
-    const newcomments = [comment, ...comments];
+	try {
+		// const router = useRouter();
+		// const { routeid } = router.query;
+		// console.log(routeid)
+		// console.log(req.body);
+		// const comment = {
+		//   ...req.body,
+		//   id: routeid,
+		//   timesince: '0 days',
+		//   replies: '0',
+		//   solved: false,
+		// };
+		// const comments = await query('postComments');
+		// const newcomments = [comment, ...comments];
 
-    console.log(newcomments);
+		// console.log(newcomments);
 
-    await writeToDb('postComments', newcomments);
-  } catch (e) {
-    res.status(400).send(e);
-  }
+		// await writeToDb('postComments', newcomments);
+		res.status(200).send('');
+	} catch (e) {
+		res.status(400).send(e);
+	}
 }
