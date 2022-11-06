d1ec30463e8ad4c80be0cd114d511fa015882c8c
sugn4 <angustodd78@gmail.com>
2022-11-06 23:51:48 +0000

    replies are pushed to db. added dummy data.

--- components/addReply.jsx
+++ components/addReply.jsx
@@ -3,21 +3,23 @@ import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import { getCurrentUser } from '/controllers/auth';
 import { comments } from 'db/postComments';
-import { axios } from 'axios';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 
 export function AddReply() {
   const { register, handleSubmit } = useForm();
-  const user = getCurrentUser();
-
+  const router = useRouter();
+  const {id} = router.query;
   const onSubmit = async (data) => {
     try{
-      const newData={
-      ...data,
-      name:getCurrentUser().name
+      const newData={name: getCurrentUser().name,
+      ...data, postId: id
+      
       }
+      console.log(newData)
       const res = await axios.post('/api/addReply', newData);
-      toast.success(user.name+JSON.stringify(data))
-      console.log(comments)
+      toast.success(JSON.stringify(res.data))
+      console.log(res)
     } catch (e) {
       console.error(e)
     }
@@ -27,11 +29,13 @@ export function AddReply() {
     <Flex width={'100%'} overflow='hidden' padding='1' flexDirection={'column'}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <FormControl isRequired>
-          <FormLabel>Comment as: <i>{user.name}</i></FormLabel>
+          <FormLabel>Comment as: <i>{getCurrentUser().name}</i></FormLabel>
         </FormControl>
-        <Textarea rounded={'md'} size='sm' placeholder='What are your thoughts?' {...register('message')} />
-        <Box padding='1'>
-          <Button type="submit">Submit</Button>
+        <Box position={'relative'}>
+        <Textarea w='100%' h='100%' rounded={'md'} size='sm' placeholder='What are your thoughts?' {...register('desc')} />
+        <Flex padding='1' justifyContent={'flex-end'}>
+          <Button zIndex={1000} position={'absolute'} bottom='3' right='1' size='sm' type="submit">Submit</Button>
+        </Flex>
         </Box>
       </form>
     </Flex>
