1290e50b0dc86eee23f93e66a7c5fdce2ff70fd6
[Dawid Wraga] <[dpwraga@gmail.com]>
2022-11-07 19:31:18 +0000

    attempt to fix json not working in prod

--- pages/api/addForumPost.js
+++ pages/api/addForumPost.js
@@ -17,7 +17,6 @@ export default async function handler(req, res) {
     const posts = await query('posts');
     const newposts = [post, ...posts];
 
-    console.log(newposts);
 
     await writeToDb('posts', newposts);
     res.status(200).send(id);
--- pages/api/addTopic.js
+++ pages/api/addTopic.js
@@ -1,24 +1,23 @@
 import query from 'controllers/query';
 import { writeToDb } from 'controllers/writeToDb';
 import { v4 as uuidv4 } from 'uuid';
+import path from 'path';
 
 export default async function handler(req, res) {
-  try {
-    console.log(req.body);
-    const id = uuidv4();
-    const topic = {
-      ...req.body,
-      numPosts: '0',
-      id,
-    };
-    const topics = await query('topics');
-    const newtopics = [topic, ...topics];
+	try {
+		const id = uuidv4();
+		const topic = {
+			...req.body,
+			numPosts: '0',
+			id,
+		};
 
-    console.log(newtopics);
+		const topics = await query('topics');
+		const newtopics = [topic, ...topics];
 
-    await writeToDb('topics', newtopics);
-    res.status(200).send(id);
-  } catch (e) {
-    res.status(400).send(e);
-  }
+		await writeToDb('topics', newtopics);
+		res.status(200).send(id);
+	} catch (e) {
+		res.status(400).send(e);
+	}
 }
