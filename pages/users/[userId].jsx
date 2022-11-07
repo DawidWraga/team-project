03758fffedca1c9400d12664f4ccdc2a48b652d7
anjanamadhavram <anjanamadhavram@gmail.com>
2022-11-07 00:24:27 +0000

    layout dashboard

--- pages/users/[userId].jsx
+++ pages/users/[userId].jsx
@@ -1,7 +1,13 @@
+import { Box } from '@chakra-ui/react';
 import { UserDashboard } from 'components/dashboards/UserDashboard';
+import { PageWrapper } from 'styles/PageWrapper';
 
 export default function IndividualUserPage(props) {
-	const {} = props;
+  const {} = props;
 
-	return <UserDashboard />;
+  return (
+    <PageWrapper w="2xl">
+      <UserDashboard />
+    </PageWrapper>
+  );
 }
