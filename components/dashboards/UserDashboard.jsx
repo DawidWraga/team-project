93f9b2883c4597e335de03fedf4ced371ac5fd9b
DawidWraga <dpwraga@gmail.com>
2022-11-07 02:26:00 +0000

    debug key loops

--- components/dashboards/UserDashboard.jsx
+++ components/dashboards/UserDashboard.jsx
@@ -6,66 +6,56 @@ import { UserTasksList } from 'components/dashboards/UserTasksList';
 import { randomNum } from 'utils/randomNum';
 import { Paper } from 'styles/Paper';
 import { PageWrapper } from 'styles/PageWrapper';
-import { redirect } from 'next/dist/server/api-utils';
-import { getParseTreeNode } from 'typescript';
 import { RepeatClockIcon } from '@chakra-ui/icons';
 
 export function UserDashboard(props) {
-  const {} = props;
+	const {} = props;
 
-  const lineChartData = getLineChartDummyData();
+	const lineChartData = getLineChartDummyData();
 
-  const pieChartData = [
-    {
-      name: 'not started',
-      value: randomNum(0, 10),
-    },
-    {
-      name: 'in progress',
-      value: randomNum(0, 10),
-    },
-    {
-      name: 'review',
-      value: randomNum(0, 10),
-    },
-    {
-      name: 'done',
-      value: randomNum(0, 10),
-    },
-  ];
+	const pieChartData = [
+		{
+			name: 'not started',
+			value: randomNum(0, 10),
+		},
+		{
+			name: 'in progress',
+			value: randomNum(0, 10),
+		},
+		{
+			name: 'review',
+			value: randomNum(0, 10),
+		},
+		{
+			name: 'done',
+			value: randomNum(0, 10),
+		},
+	];
 
-  return (
-    <div>
-      <Grid
-        templateColumns="repeat(3, 1fr)"
-        templateRows="repeat(2, 40vh)"
-        gap={5}
-        placeItems="stretch"
-      >
-        <GridItem colSpan="2" overflowY={'auto'}>
-          <Paper variant="elevated">
-            <UserTasksList tasks={tasks} />
-          </Paper>
-        </GridItem>
-        <GridItem bgColor="blue.100" h="100%">
-          <Paper variant="elevated">
-            <PieChart pieChartData={pieChartData} />
-          </Paper>
-        </GridItem>
-        <GridItem colSpan={3} bgColor="green.100">
-          <Paper w="100%" h="max-content" variant="elevated">
-            <LineChart data={lineChartData} />
-          </Paper>
-        </GridItem>
-      </Grid>
-    </div>
-  );
+	return (
+		<div>
+			<Grid
+				templateColumns="repeat(3, 1fr)"
+				templateRows="repeat(2, 40vh)"
+				gap={5}
+				placeItems="stretch"
+			>
+				<GridItem colSpan="2" overflowY={'auto'}>
+					<Paper variant="elevated">
+						<UserTasksList tasks={tasks} />
+					</Paper>
+				</GridItem>
+				<GridItem bgColor="blue.100" h="100%">
+					<Paper variant="elevated">
+						<PieChart pieChartData={pieChartData} />
+					</Paper>
+				</GridItem>
+				<GridItem colSpan={3} bgColor="green.100">
+					<Paper w="100%" h="max-content" variant="elevated">
+						<LineChart data={lineChartData} />
+					</Paper>
+				</GridItem>
+			</Grid>
+		</div>
+	);
 }
-
-// const tasks = [
-//   { title: 'Email boss man', status: 'Done', due: '11/13/2022' },
-//   { title: 'Email boss man', status: 'In progress', due: '11/13/2022' },
-//   { title: 'Email boss man', status: 'Not started', due: '11/13/2022' },
-//   { title: 'Email boss man', status: 'Done', due: '11/13/2022' },
-//   { title: 'Email boss man', status: 'In progress', due: '11/13/2022' },
-// ];
