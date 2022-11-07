cd8941e29fbaa1b43183b75d845a3067d14ef1c6
[Dawid Wraga] <[dpwraga@gmail.com]>
2022-11-07 16:26:06 +0000

    fix write to json bug

--- controllers/query.ts
+++ controllers/query.ts
@@ -1,14 +1,15 @@
 import path from 'path';
 import fs from 'fs/promises';
-import { setTimeoutPromise } from 'utils/setTimeoutPromise';
 
 export default async function query(table: string) {
-	const jsonDirectory = path.join(process.cwd(), `db/${table}.json`);
-	const jsonData = await fs.readFile(jsonDirectory, 'utf8');
-	const objectData = JSON.parse(jsonData);
+	try {
+		const jsonDirectory = path.join(process.cwd(), `db/${table}.json`);
+		const jsonData = await fs.readFile(jsonDirectory, 'utf8');
+		const objectData = JSON.parse(jsonData);
 
-	// simulate response delay
-	// await setTimeoutPromise(50);
-
-	return objectData;
+		return objectData;
+	} catch (e) {
+		console.error(e);
+		return false;
+	}
 }
