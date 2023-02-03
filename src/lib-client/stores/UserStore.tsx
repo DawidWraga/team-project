d1f71e86a9443908b8a2d22f93c819d52d7526ae
DawidWraga <dpwraga@gmail.com>
2023-02-03 15:38:12 +0000

    fix: user store mistake

--- src/lib-client/stores/UserStore.tsx
+++ src/lib-client/stores/UserStore.tsx
@@ -4,10 +4,15 @@ export type Roles = 'admin' | 'client' | 'contractor' | 'emp';
 export const roles: Roles[] = ['admin', 'client', 'contractor', 'emp'];
 
 export interface IUserStore {
-  username: string;
-  setUsername: (username: string) => void;
+  userRole: Roles;
+  setUserRole: (userRole: Roles) => void;
+  resourceId: number;
+  setResourceId: (id: number) => void;
 }
+5;
 export const useUserStore = createStore<IUserStore>('User', (set) => ({
-  username: '',
-  setUsername: (username: string) => set({ username }),
+  userRole: 'admin',
+  setUserRole: (userRole: Roles) => set({ userRole }),
+  resourceId: 1,
+  setResourceId: (id: number) => set({ resourceId: id }),
 }));
