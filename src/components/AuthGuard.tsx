bffbffe9d3f9a0db4f14935810b7596ee336cda5
DawidWraga <dpwraga@gmail.com>
2023-02-10 22:22:02 +0000

    feat: protected routes

---
+++ src/components/AuthGuard.tsx
@@ -0,0 +1,33 @@
+import { useSession } from 'next-auth/react';
+import { Loading } from '@saas-ui/react';
+import { useRouter } from 'next/router';
+interface IProps {
+  children: React.ReactNode;
+}
+
+export default function AuthGuard(props: IProps) {
+  const { children } = props;
+  const router = useRouter();
+
+  const { status } = useSession();
+
+  // ======= HANDLE LOADING ======
+  if (status === 'loading') {
+    return <Loading variant="fullscreen" />;
+  }
+
+  // ======= HANDLE UNAUTHENTICATED ======
+  if (status === 'unauthenticated') {
+    if (['/register', '/auth'].includes(router.asPath)) {
+      return <>{children}</>;
+    } else {
+      router.replace('/auth');
+      return <Loading variant="fullscreen" />;
+    }
+  }
+
+  // ======= HANDLE AUTHENTICATED ======
+  if (status === 'authenticated') {
+    return <>{children}</>;
+  } else return <>{children}</>; //there are only 3 status optios therefore the else will never run. Here purely for type safety
+}
