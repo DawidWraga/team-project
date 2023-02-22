0d6a71b68594c36ffa2a4e18a079fd1c137ced67
DawidWraga <dpwraga@gmail.com>
2023-02-22 19:26:52 +0000

    fix: refetch user every minute

--- src/lib-client/hooks/useUser.tsx
+++ src/lib-client/hooks/useUser.tsx
@@ -29,7 +29,7 @@ export function useUser() {
         },
       },
     },
-    staleTime: 1000 * 60, // 1 minute
+    refetchInterval: 1000 * 60, // 1 minute
     enabled: Boolean(session.status === 'authenticated') && Boolean(userEmail),
   });
 
