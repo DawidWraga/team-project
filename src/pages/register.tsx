e926c4e4b449ae8e5a3c360acb69c9061db48edb
DawidWraga <dpwraga@gmail.com>
2023-02-23 11:46:35 +0000

    fix: register show password error

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -12,53 +12,54 @@ export const emailSchema = z
   .email()
   .refine((arg) => arg.includes('@make-it-all.co.uk'), 'Only internal emails allowed');
 
-const createUserSchema = z
-  .object({
-    fullName: z.string().min(5),
-    email: emailSchema,
-    password: z
-      .string()
-      .optional()
-      .superRefine((password, ctx) => {
-        let fullMsg = 'Password must contain at least';
-        const originalMsg = fullMsg;
-
-        // loop through requirements & append error message to fullMsg message if not met
-        [
-          { regex: /^.{8,}$/, msg: '8 characters' },
-          { regex: /[@$!%*?&]{1,}/, msg: 'one symbol' },
-          { regex: /[a-z]{1,}/, msg: 'one lowercase letter' },
-          { regex: /[A-Z]{1,}/, msg: 'one uppercase letter' },
-          { regex: /[0-9]{1,}/, msg: 'one number' },
-        ].forEach(({ regex, msg }) => {
-          const result = z.string().regex(regex).safeParse(password);
-          if (!result.success) fullMsg += ` ${msg},`;
-        });
-
-        // if any requirements are not met, set error
-        if (fullMsg !== originalMsg) {
-          ctx.addIssue({
-            code: 'custom',
-            message: fullMsg.replace(/\,(?=[^,]*$)/, '.'),
-          });
-        }
-      }),
-    confirmPassword: z.string().optional(),
-  })
-  .superRefine(({ password, confirmPassword }, ctx) => {
-    // check if passwords match
-    if (confirmPassword !== password) {
-      ctx.addIssue({
-        code: 'custom',
-        path: ['confirmPassword'],
-        message: 'The passwords did not match',
-      });
-    }
-  });
-
 export default function RegisterPage(props: any) {
   const {} = props;
   const router = useRouter();
+
+  const createUserSchema = z
+    .object({
+      fullName: z.string().min(5),
+      email: emailSchema,
+      password: z
+        .string()
+        .optional()
+        .superRefine((password, ctx) => {
+          let fullMsg = 'Password must contain at least';
+
+          // loop through requirements & append error message to fullMsg message if not met
+          [
+            { regex: /^.{8,}$/, msg: '8 characters' },
+            { regex: /[@$!%*?&]{1,}/, msg: 'one symbol' },
+            { regex: /[a-z]{1,}/, msg: 'one lowercase letter' },
+            { regex: /[A-Z]{1,}/, msg: 'one uppercase letter' },
+            { regex: /[0-9]{1,}/, msg: 'one number' },
+          ].forEach(({ regex, msg }) => {
+            const result = z.string().regex(regex).safeParse(password);
+            if (!result.success) {
+              fullMsg += ` ${msg},`;
+            }
+          });
+
+          // if any requirements are not met, set error
+          if (fullMsg !== 'Password must contain at least') {
+            ctx.addIssue({
+              code: 'custom',
+              message: fullMsg.replace(/\,(?=[^,]*$)/, '.'),
+            });
+          }
+        }),
+      confirmPassword: z.string().optional(),
+    })
+    .superRefine(({ password, confirmPassword }, ctx) => {
+      // check if passwords match
+      if (confirmPassword !== password) {
+        ctx.addIssue({
+          code: 'custom',
+          path: ['confirmPassword'],
+          message: 'The passwords did not match',
+        });
+      }
+    });
 
   const isHydrated = useIsHydrated();
 
